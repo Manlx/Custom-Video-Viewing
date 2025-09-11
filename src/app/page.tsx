464fa97fcd9e64ece7f7d7d2f3906180072e351b
Manuel Nunes <marnununes@gmail.com>
2025-09-11 02:33:24 +0200

    Change constant to rather reference config;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import { GetPagesAndVideos } from "@/helpers";
 import { VideoPaginator } from "./VideoPagination";
+import { Suspense } from "react";
+import { Config } from "@/config";
 
-const pages = GetPagesAndVideos('D:/Screen Recording AMD/public/')
+const pages = GetPagesAndVideos(Config.PublicFolderPath)
 
 export default function Video() {
   return (
-    <VideoPaginator pages={pages}></VideoPaginator>
+    <Suspense fallback={<div>Loading search results...</div>}>
+
+      <VideoPaginator pages={pages}></VideoPaginator>
+    </Suspense>
   )
 }
 //ffmpeg -hwaccel d3d11va -i "Hell Let Loose_replay_2025.01.18-01.39.mp4" -c:v h264_amf -crf 1 -c:a copy output2.mp4
